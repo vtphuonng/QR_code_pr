b2bf365e73cb6ab8090cd64791355893504b9f50
= <=>
2023-11-29 14:56:00 +0700

    update static file

--- qr_pr/settings.py
+++ qr_pr/settings.py
@@ -111,3 +111,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = 'media/'
+STATIC_ROOT = BASE_DIR / "staticfiles"  # Update the path as needed
